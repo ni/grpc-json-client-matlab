ca848c6e749e4ce05efd3df0488d6f4a3e8bec9d
Sean Moore <sean.moore@ni.com>
2022-03-10 11:11:20 -0600

    make examples more generic

--- examples/asynccallexample.m
+++ examples/asynccallexample.m
@@ -1,16 +1,9 @@
-service = 'nirfsa_grpc.NiRFSA';
-resourceName = 'VST2_01';
+service = 'helloworld.Greeter';
 
-client = nigrpcjsonclient.Session('localhost:31763');
+client = nigrpcjsonclient.Session('localhost:50051');
 
-request = jsonencode(struct('session_name', 'Demo', 'resource_name', resourceName));
-initTag = client.startasynccall(service, 'Init', request, -1);
+request = jsonencode(struct('name', 'MATLAB'));
+tag = client.startasynccall(service, 'SayHello', request, -1);
 
-request = jsonencode(struct('vi', struct('name', 'Demo')));
-closeTag = client.startasynccall(service, 'Close', request, -1);
-
-response = client.finishasynccall(initTag, -1);
-disp(response);
-
-response = client.finishasynccall(closeTag, -1);
+response = client.finishasynccall(tag, -1);
 disp(response);
--- examples/blockingcallexample.m
+++ examples/blockingcallexample.m
@@ -1,13 +1,7 @@
-service = 'nirfsa_grpc.NiRFSA';
-resourceName = 'VST2_01';
+service = 'helloworld.Greeter';
 
-client = nigrpcjsonclient.Session('localhost:31763');
+client = nigrpcjsonclient.Session('localhost:50051');
 
-request = jsonencode(struct('session_name', 'Demo', 'resource_name', resourceName));
-response = client.blockingcall(service, 'Init', request, -1);
-disp(response);
-response = jsondecode(response);
-
-request = jsonencode(struct('vi', response.vi));
-response = client.blockingcall(service, 'Close', request, -1);
+request = jsonencode(struct('name', 'MATLAB'));
+response = client.blockingcall(service, 'SayHello', request, -1);
 disp(response);
